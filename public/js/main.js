7386163908f877b703b01e5c6e23c0a2787a1c4e
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-11-30 22:25:49 -0200

    a lot of improvements

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,10 @@
 (function() {
   window.sr = new scrollReveal();
-  var circleMenu = document.querySelector('.circle-menu');
-  var circleMenuLinks = circleMenu.querySelectorAll('li');
-  circleMenuLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-      e.currentTarget.querySelector('a').click();
-    });
-  });
-  circleMenu.addEventListener('click', function(e) {
-    var classList = e.currentTarget.querySelector('ul').classList;
-    if (classList.contains('show-menu')) {
-      classList.remove('show-menu');
-    } else {
-      classList.add('show-menu');
-    }
+  cssCircleMenu('.js-menu');
+  var toggle = document.querySelector('.navbar-toggle');
+  var collapse = document.querySelector('.navbar-collapse');
+  toggle.addEventListener('click', function() {
+    collapse.classList.toggle('in');
   });
   var msg = '  _   _  ___    ___ __      __ ___  ___  ___   ___ __   __\n';
       msg += ' | | | ||   \\  / __|\\ \\    / /| __|| _ )|   \\ | __|\\ \\ / /\n';
