079268de35d6a35a88b1435bb0395e4ab188e99c
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-12-04 17:48:43 -0200

    Deploying Underground WebDev

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function() {
+  cssCircleMenu('.js-menu');
+  var toggle = document.querySelector('.navbar-toggle');
+  var collapse = document.querySelector('.navbar-collapse');
+  toggle.addEventListener('click', function() {
+    collapse.classList.toggle('in');
+  });
+  var msg = '  _   _  ___    ___ __      __ ___  ___  ___   ___ __   __\n';
+      msg += ' | | | ||   \\  / __|\\ \\    / /| __|| _ )|   \\ | __|\\ \\ / /\n';
+      msg += ' | |_| || |) || (_ | \\ \\/\\/ / | _| | _ \\| |) || _|  \\ \V / \n';
+      msg += '  \\___/ |___/  \\___|  \\_/\\_/  |___||___/|___/ |___|  \\_/    \n';
+      msg += ' Powered by @crp_underground\n';
+  console.log(msg);
+})();
