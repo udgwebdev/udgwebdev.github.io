96f026c9bb21a035005923332e5ffc05e6c0bec8
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-12-07 17:31:21 -0200

    improving sidebar and added more animation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,9 +3,23 @@
   new Tooltip().init({ opacity: '.90', hideOnMouseOut: true });
   var toggle = document.querySelector('.navbar-toggle');
   var collapse = document.querySelector('.navbar-collapse');
+  var sidemenu = document.querySelector('[data-sticky-sidemenu]');
   toggle.addEventListener('click', function() {
     collapse.classList.toggle('in');
   });
+  if (sidemenu) {
+    window.addEventListener('scroll', function(e) {
+      const isSticky = sidemenu.classList.contains('sticky');
+      if (window.scrollY >= 1000 && !isSticky) {
+        sidemenu.classList.add('sticky');
+        sidemenu.classList.add('fadeInDown');
+      }
+      if (window.scrollY < 1000 && isSticky) {
+        sidemenu.classList.remove('sticky');
+        sidemenu.classList.remove('fadeInDown');
+      }
+    });
+  }
   var msg = '  _   _  ___    ___ __      __ ___  ___  ___   ___ __   __\n';
       msg += ' | | | ||   \\  / __|\\ \\    / /| __|| _ )|   \\ | __|\\ \\ / /\n';
       msg += ' | |_| || |) || (_ | \\ \\/\\/ / | _| | _ \\| |) || _|  \\ \V / \n';
