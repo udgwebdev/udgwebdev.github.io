5f1936b1c83b97b6a4f75df57b8c910bb9d02121
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2018-02-02 11:58:46 -0200

    Deploying Underground WebDev

--- assets/js/plugins/circlemenu.js
+++ assets/js/plugins/circlemenu.js
@@ -2,11 +2,11 @@
   'use strict';
 
   window.cssCircleMenu = function(el) {
-    var $menu = document.querySelector(el);
-    var $menuToggle = $menu ? $menu.querySelector('.js-menu-toggle') : undefined;
-    var $menuMask = $menu ? $menu.querySelector('.js-menu-mask') : undefined;
+    var menu = document.querySelector(el);
+    var menuToggle = menu.querySelector('.js-menu-toggle');
+    var menuMask = menu.querySelector('.js-menu-mask');
 
-    if (!$menu || !$menuToggle || !$menuMask) {
+    if (!menu || !menuToggle || !menuMask) {
       throw new Error('Invalid elements, check the structure.');
     } else {
       init();
@@ -18,28 +18,30 @@
     };
 
     function init() {
-      $menuToggle.addEventListener('click', function(e) {
+      menuMask.addEventListener('click', function(e) {
+        e.preventDefault();
+        closeMenu();
+      });
+      menuToggle.addEventListener('click', function(e) {
         e.preventDefault();
         toggleMenu();
       });
     }
 
     function toggleMenu() {
-      $menuToggle.classList.contains('is-active')
-        ? closeMenu()
-        : openMenu();
+      menuToggle.classList.contains('is-active') ? closeMenu() : openMenu();
     }
 
     function openMenu() {
-      $menu.classList.add('is-active');
-      $menuToggle.classList.add('is-active');
-      $menuMask.classList.add('is-active');
+      menu.classList.add('is-active');
+      menuToggle.classList.add('is-active');
+      menuMask.classList.add('is-active');
     }
 
     function closeMenu() {
-      $menu.classList.remove('is-active');
-      $menuToggle.classList.remove('is-active');
-      $menuMask.classList.remove('is-active');
+      menu.classList.remove('is-active');
+      menuToggle.classList.remove('is-active');
+      menuMask.classList.remove('is-active');
     }
   };
 })(window);
